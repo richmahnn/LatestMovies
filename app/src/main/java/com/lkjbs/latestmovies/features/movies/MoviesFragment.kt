e438a8beae8f5401d80b636627d65f92d53d75ca
lubulwa <lubulwaric@gmail.com>
2019-08-10 23:39:54 +0300

    Added login feature

--- app/src/main/java/com/lkjbs/latestmovies/features/movies/MoviesFragment.kt
+++ app/src/main/java/com/lkjbs/latestmovies/features/movies/MoviesFragment.kt
@@ -1,7 +1,11 @@
 package com.lkjbs.latestmovies.features.movies
 
+import com.lkjbs.latestmovies.R
 import com.lkjbs.latestmovies.core.platform.BaseFragment
 
 class MoviesFragment : BaseFragment() {
 
+    
+
+    override fun layoutId(): Int = R.layout.fragment_movies
 }
